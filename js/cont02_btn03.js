68a8a73ee8b52835da7781a23f3437ad7cd153e0
johanmorales <jhellmetal2000@gmail.com>
2017-09-06 11:57:39 -0500

    Primer commit a repositorio organizado, primera version para revision por nayfer.

---
+++ js/cont02_btn03.js
@@ -0,0 +1,9 @@
+$(function(){
+	console.log("Image lens prueba...")
+
+	//http://www.elevateweb.co.uk/image-zoom/examples
+	$(".cont_tab_size02-2").elevateZoom({
+		zoomWindowPosition: 10,
+  		cursor: "crosshair"
+	});
+})
